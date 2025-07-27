f9930116577c79c09fd56448d1e767da69a118b4
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-27 16:02:57 +0530

    Add MainHeaderBackground component and refactor MainHeader to include background SVG; clean up globals.css

--- components/main-header.js
+++ components/main-header.js
@@ -1,26 +1,31 @@
 import Link from "next/link";
-import logoImg from '@/assets/logo.png'
+import logoImg from "@/assets/logo.png";
 import Image from "next/image";
-import classes from './main-header.module.css'
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
-export default function MainHeader(){
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
 
-
-    return <header className={classes.header}>
-        <Link className={classes.logo} href='/'>
-        <Image src={logoImg} alt="A plate with food on it"/>
-        NextLevel Food
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
         </Link>
 
         <nav className={classes.nav}>
-            <ul>
-                <li>
-                    <Link href="/meals">Browse Meals</Link>
-                </li>
-                 <li>
-                    <Link href="/community">Foodies community</Link>
-                </li>
-            </ul>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies community</Link>
+            </li>
+          </ul>
         </nav>
-    </header>
+      </header>
+    </>
+  );
 }
