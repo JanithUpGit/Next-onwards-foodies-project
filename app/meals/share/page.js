95c32e555abc26c8c05218296fe243dba0cb7ea7
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-29 17:15:27 +0530

    Refactor ShareMealPage and MealsFormSubmit components; update shareMeal function for improved validation and state management

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import ImagePicker from '@/components/meals/image-pieker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
